fe0062ec0c87f3a51e47500180e108d09c7eac6a
Sepehr Mohammadi <sepehr.mohammadi@.dentsu.com>
2023-03-23 08:27:57 +0100

    change content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img
+          src="https://www.dentsu.com/assets/images/main-logo-alt.png"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>Integrated Marketing Solutions & Agency Services</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.dentsu.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Read more
         </a>
       </header>
     </div>
